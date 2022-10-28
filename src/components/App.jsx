35bd42b8a61c1a8a8e3ba427fa812bbce035a977
Yuliia-Bakalova <98843051+Yuliia-Bakalova@users.noreply.github.com>
2022-10-28 14:53:02 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,34 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 class App extends Component {
     state = {
-        filter: '',
+        
         contacts: [
             { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
             { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
             { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
             { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-        ],
+        ],    
+    filter: '',
     };
+
+
+    componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const savedContacts = JSON.parse(contacts);
+
+    if (savedContacts) {
+      this.setState({ contacts: savedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const nextContacts = this.state.contacts;
+    const prevContacts = prevState.contacts;
+
+    if (nextContacts !== prevContacts) {
+      localStorage.setItem('contacts', JSON.stringify(nextContacts));
+    }
+  }
 
     formSubmitHemdler = contact => {
         contact.id = nanoid();
